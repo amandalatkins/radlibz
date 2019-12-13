8ae9d2a468069eebecfa658e83dcd7e9221a31fb
marwan jassim <marwanjassim87@gmail.com>
2019-12-13 13:59:36 -0800

    we have a dropdown of multiple accents and ways to make text to speach

--- voicegenerator.js
+++ voicegenerator.js
@@ -1,41 +1,6 @@
-var queryURL = http://madlibz.herokuapp.com/api/random
-console.log(responce)
-
-<form id="forcountry">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="adverb">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-<form id="adjective">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="animal">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="verbing">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="verb">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="forcountry">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
-
-<form id="forcountry">
-        <label for="word-input">Add word</label>
-        <input type="text" id="word-input">
-        <br>
+ //Populate voice selection dropdown
+ var voicelist = responsiveVoice.getVoices();
+ var vselect = $("#voiceselection");
+ $.each(voicelist, function() {
+         vselect.append($("<option />").val(this.name).text(this.name));
+ });
