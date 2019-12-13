38b41180f352b0e935eea3ce8dc17d2613702c96
Amanda Atkins <amanda@digitalrainstorm.com>
2019-12-13 11:19:27 -0800

    Build out functionality to loop through blanks, append word type to screen, accept user input and store in userResponses array, and end game when all blanks have been filled.

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
 var playBtn = $('#playBtn');
 var wordTypeContainer = $('#wordTypeContainer');
 var wordInput = $('#wordInput');
-var userButton = $('.userWord');
-var suggestionButton = $('.suggestWord');
+var userButton = $('#userWord');
+var suggestionContainer = $('#wordSuggestion');
+var introScreen = $('#intro');
+var questionScreen = $('#question');
 
 
 var madApi = "https://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25";
 
-var madObj;
+var blanks;
+var sentences;
+var title;
+var userResponses = [];
 var curIndex = 0;
 
-playBtn.on('click',function() {
-    $(this).hide();
-    $('.question').show();
+function startGame() {
+    introScreen.hide();
+    questionScreen.show();
     $.ajax({
         url: madApi,
         method: "GET"
     }).then(function(response) {
-        madObj = response;
-        console.log(madObj);
+        blanks = response.blanks;
+        sentences = response.value;
+        title = response.title;
         loadWordQuestion();
     });
-});
+};
 
 function loadWordQuestion() {
-    wordTypeContainer.text(madObj.blanks[curIndex]);
+    if (curIndex < blanks.length) {
+        console.log(userResponses);
+        wordTypeContainer.text(blanks[curIndex]);
+    } else {
+        endGame();
+    }
 }
+
+function captureUserInput(e) {
+    if (e.keyCode && e.keyCode !== 13) {
+        return;
+    }
+    userResponses.push(wordInput.val());
+    wordInput.val('');
+    curIndex++;
+    loadWordQuestion();
+}
+
+function captureButtonInput() {
+    if ($(this).matches('button')) {
+        userResponses.push($(this).val());
+        wordInput.val('');
+        curIndex++;
+        loadWordQuestion();
+    }
+}
+
+function endGame() {
+    console.log(blanks);
+    console.log(userResponses);
+    questionScreen.hide();
+    alert('game over');
+}
+
+// Event Listeners
+playBtn.on('click',startGame);
+suggestionContainer.on('click',captureButtonInput);
+userButton.on('click',captureUserInput);
+wordInput.on('keyup',captureUserInput);
