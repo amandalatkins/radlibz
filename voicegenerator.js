517bb2e81a528d8156f4389416740f84bfa07576
marwan jassim <marwanjassim87@gmail.com>
2019-12-13 14:18:38 -0800

    Js codes moved from text.html to voicegenerator.JS

--- voicegenerator.js
+++ voicegenerator.js
@@ -1,6 +1,11 @@
- //Populate voice selection dropdown
- var voicelist = responsiveVoice.getVoices();
- var vselect = $("#voiceselection");
- $.each(voicelist, function() {
-         vselect.append($("<option />").val(this.name).text(this.name));
- });
+$("#voiceselection").on("click", function(){
+
+
+        
+   //Populate voice selection dropdown
+    var voicelist = responsiveVoice.getVoices();
+    var vselect = $("#voiceselection");
+        $.each(voicelist, function() {
+                vselect.append($("<option />").val(this.name).text(this.name));
+                });
+})
