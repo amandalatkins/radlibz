1b2563ba9d3c050bccf5ecc72d163e16ecc18029
marwan jassim <marwanjassim87@gmail.com>
2019-12-13 12:43:23 -0800

    created 2 files test.html and voicegenerator.js

---
+++ storygenerator.js
@@ -0,0 +1,41 @@
+var queryURL = http://madlibz.herokuapp.com/api/random
+console.log(responce)
+
+<form id="forcountry">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="adverb">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+<form id="adjective">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="animal">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="verbing">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="verb">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="forcountry">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
+
+<form id="forcountry">
+        <label for="word-input">Add word</label>
+        <input type="text" id="word-input">
+        <br>
