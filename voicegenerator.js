8611368ada98d1b91ab2c6f895051515e9c3264b
marwan jassim <marwanjassim87@gmail.com>
2019-12-15 14:32:04 -0800

    cleaned up and added comments

--- voicegenerator.js
+++ voicegenerator.js
@@ -1,11 +1,23 @@
-$("#voiceselection").on("click", function(){
+function speakText() {
+    // Get text from textbox.
+    var text = $('#text').val();
 
+    // Get selected voice from dropdown
+    var voice = $('#voiceselection').val()
 
-        
-   //Populate voice selection dropdown
-    var voicelist = responsiveVoice.getVoices();
-    var vselect = $("#voiceselection");
-        $.each(voicelist, function() {
-                vselect.append($("<option />").val(this.name).text(this.name));
-                });
-})
+    // Speak it
+    responsiveVoice.speak(text, voice)
+}
+
+// The rest of this runs when the page opens
+
+// Get list of voices
+var voicelist = responsiveVoice.getVoices();
+
+// Get voice selection drop down
+var vselect = $("#voiceselection");
+
+// Add an option for each voice to the drop down
+$.each(voicelist, function() {
+    vselect.append($("<option />").val(this.name).text(this.name));
+});
